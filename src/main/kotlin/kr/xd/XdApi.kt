19406793d7909bb318a0876293faa5535ab70a95
h-beeen <happybean@hectodata.co.kr>
2024-10-03 14:51:20 +0900

    chore: initial commit

---
+++ src/main/kotlin/kr/xd/XdApi.kt
@@ -0,0 +1,11 @@
+package kr.xd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XdApplication
+
+fun main(args: Array<String>) {
+    runApplication<XdApplication>(*args)
+}
